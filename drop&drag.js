29b10da4ff6fdc3501f94db02205000a95ac9153
Wisambebar1 <wisambebar@gmail.com>
2024-10-29 13:13:05 +0200

    change background of the card

--- drop&drag.js
+++ drop&drag.js
@@ -4,7 +4,14 @@ const dropzone = document.getElementById('dropzone');
 card.addEventListener('dragstart', (e) => {
     e.dataTransfer.effectAllowed = 'move';
 });
-
+card.addEventListener('click', () => {
+    if (card.style.display === 'none' || card.style.display === '') {
+        card.src = customImageUrl; // Set the custom image source
+        card.style.display = 'block'; // Show the image
+    } else {
+        image.style.display = 'none'; // Hide the image
+    }
+})
 dropzone.addEventListener('dragover', (e) => {
     e.preventDefault();
 });
