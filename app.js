50299253234991fcd4396582d3747d2e00ec73dd
WebNinja95 <avivsalem95@gmail.com>
2024-10-30 17:21:33 +0200

    almost dont

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
-function toggleMenu() {
+
+const menu = document.getElementById('menu-icon');
+const rules = document.getElementById('rules');
+
+menu.addEventListener('click',()=>{
     const navLinks = document.querySelector('.nav-links');
     navLinks.classList.toggle('active');
-}
+})
+
+rules.addEventListener('click',()=>{
+    window.location.href = 'learn.html';
+})
